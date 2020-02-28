ab40b29015a9f773c688b54cb8ff3cdf2e6eb5ee
coppey <sacha.coppey@epfl.ch>
2020-02-28 22:53:39 +0100

    added a button that show the current localization

--- app/src/main/java/ch/epfl/sdp/MapsActivity.java
+++ app/src/main/java/ch/epfl/sdp/MapsActivity.java
@@ -1,17 +1,42 @@
 package ch.epfl.sdp;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private Location currentLocation;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private String bestProvider;
+    private LocationListener locationListener;
+    private Marker marker;
+
+    public static double netlistentime = 0 * 60 * 1000; // minutes * 60 sec/min * 1000 for milliseconds
+    public static double netlistendistance = 0 * 1609.344; // miles * conversion to meters
+    public static double gpslistentime = 30 * 60 * 1000; // minutes * 60 sec/min * 1000 for milliseconds
+    public static double gpslistendistance = 0 * 1609.344; // miles * conversion to meters
+
 
     private GoogleMap mMap;
 
@@ -19,12 +44,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        Button button = (Button) findViewById(R.id.update_loc);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                updatePosition();
+            }
+        });
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // setup bestProvider
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        bestProvider = locationManager.getBestProvider(criteria, true);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location latestlocation) {
+                // Called when a new location is found by the network location provider.
+                currentLocation.set(latestlocation);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(MapsActivity.this);
+
     }
 
+    private void updatePosition() {
+        if (marker != null) {
+            marker.remove();
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            //return;
+        }
+        boolean isNetEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if(isNetEnabled) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, (long) netlistentime, (float) netlistendistance, locationListener);
+        }
+        if(isGpsEnabled) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, (long) gpslistentime, (float) gpslistendistance, locationListener);
+        }
+
+        currentLocation = locationManager.getLastKnownLocation(bestProvider);
+        LatLng myPos = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        marker = mMap.addMarker(new MarkerOptions().position(myPos).title("My position"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPos));
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,9 +125,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        updatePosition();
     }
 }
