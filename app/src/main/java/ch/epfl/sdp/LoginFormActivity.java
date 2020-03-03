cf02bf1de271f76503a8792ec7f54f46b0d14cc8
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-03-03 17:18:12 +0100

    login works connected with the firebase

--- app/src/main/java/ch/epfl/sdp/LoginFormActivity.java
+++ app/src/main/java/ch/epfl/sdp/LoginFormActivity.java
@@ -16,7 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginformActivity extends AppCompatActivity {
+public class LoginFormActivity extends AppCompatActivity {
     EditText lusername, lemail,lpassword;
     Button lLoginButton;
     FirebaseAuth firebaseAuth;
@@ -31,44 +31,44 @@ public class LoginformActivity extends AppCompatActivity {
         lpassword = findViewById(R.id.passwordlog);
         firebaseAuth = FirebaseAuth.getInstance();
         lLoginButton = findViewById(R.id.loginButton);
-
-        lLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = lemail.getText().toString().trim();
-                String password = lpassword.getText().toString().trim();
-
-                if (TextUtils.isEmpty(email)) {
-                    lemail.setError("email is required");
-                    return;
-                }
-                if (TextUtils.isEmpty((password))) {
-                    lpassword.setError("password is required");
-                    return;
-                }
-                if (password.length() < 8) {
-                    lpassword.setError("password length has to be greater than 8 Characters");
-                    return;
-                }
-
-                //authentificate the user
-                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(LoginformActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        } else
-                            Toast.makeText(LoginformActivity.this, "Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-
-                    }
-                });
-            }
-        });
     }
 
-    public void createAccountBtn_OnClick(View view) {
-        startActivity(new Intent(this, RegisterformActivity.class));
+   public void createAccountBtn_OnClick(View view) {
+        startActivity(new Intent(this, RegisterFormActivity.class));
+        finish();
+    }
+
+    public void loginBtn_OnClick(View view) {
+        String email = lemail.getText().toString().trim();
+        String password = lpassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            lemail.setError("email is required");
+            return;
+        }
+
+        if (TextUtils.isEmpty((password))) {
+            lpassword.setError("password is required");
+            return;
+        }
+
+        if (password.length() < 8) {
+            lpassword.setError("password length has to be greater than 8 Characters");
+            return;
+        }
+
+        //authentificate the user
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginFormActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(LoginFormActivity.this, "Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
