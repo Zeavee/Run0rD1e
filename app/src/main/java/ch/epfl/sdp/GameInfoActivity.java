e0a3930694bea951091e615d23bfdd59f5355d0e
kangpl <peilin.kang@epfl.ch>
2020-03-05 00:20:32 +0100

    hit button and then go to game info view

---
+++ app/src/main/java/ch/epfl/sdp/GameInfoActivity.java
@@ -0,0 +1,13 @@
+package ch.epfl.sdp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_info);
+    }
+}
--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -2,7 +2,10 @@ package ch.epfl.sdp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Locate the button in activity_main.xml
+        Button button = findViewById(R.id.mainGoButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, GameInfoActivity.class);
+        startActivity(intent);
     }
 }
