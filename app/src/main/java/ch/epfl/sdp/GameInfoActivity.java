0fbb0b3cdd12668133268488e54b0d75736c4706
kangpl <peilin.kang@epfl.ch>
2020-03-05 00:48:32 +0100

    Update the username in the UI from the player field

--- app/src/main/java/ch/epfl/sdp/GameInfoActivity.java
+++ app/src/main/java/ch/epfl/sdp/GameInfoActivity.java
@@ -13,8 +13,13 @@ public class GameInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_info);
 
-        TextView textView = findViewById(R.id.healthpoint_text);
+
         Player player = new Player(new GeoPoint(toRadians(6.14308), toRadians(46.21023)), 1.0, "admin", "admin@epfl.ch");
-        textView.setText(String.valueOf(player.healthPoints));
+
+        TextView username = findViewById(R.id.username_text);
+        username.setText(String.valueOf(player.username));
+
+        TextView healthPoint = findViewById(R.id.healthpoint_text);
+        healthPoint.setText(String.valueOf(player.healthPoints));
     }
 }
