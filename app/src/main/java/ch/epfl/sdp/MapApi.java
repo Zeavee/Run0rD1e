38f2c53ef10f051622537f00bd99ecc2c415220b
coppey <sacha.coppey@epfl.ch>
2020-03-05 21:53:36 +0100

    refactor the code to have a nice MapApi

--- app/src/main/java/ch/epfl/sdp/MapApi.java
+++ app/src/main/java/ch/epfl/sdp/MapApi.java
@@ -1,9 +1,11 @@
 package ch.epfl.sdp;
 
+import android.app.Activity;
 import android.location.Location;
 
 public interface MapApi {
+
     public Location getCurrentLocation();
 
-    public void updatePosition();
+    public void updatePosition(Activity activity);
 }
--- app/src/main/java/ch/epfl/sdp/MapsActivity.java
+++ app/src/main/java/ch/epfl/sdp/MapsActivity.java
@@ -1,39 +1,21 @@
 package ch.epfl.sdp;
 
-import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, MapApi {
-
-    private Location currentLocation = new Location("Temp_Loc");
-    private LocationManager locationManager;
-    private Criteria criteria;
-    private String bestProvider;
-    private LocationListener locationListener;
-    private Marker marker;
-    private GoogleMap mMap;
-
-    public static double listenTime = 1000; // milliseconds
-    public static double listenDistance = 5; // meters
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private MapApi mapApi;
+    private Activity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,80 +25,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Button button = findViewById(R.id.update_loc);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                updatePosition();
+                mapApi.updatePosition(activity);
             }
         });
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        // setup bestProvider
-        criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        bestProvider = locationManager.getBestProvider(criteria, true);
-
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location latestlocation) {
-                // Called when a new location is found by the network location provider.
-                currentLocation = latestlocation;
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-            @Override
-            public void onProviderEnabled(String provider) {}
-
-            @Override
-            public void onProviderDisabled(String provider) {}
-        };
+        mapApi = new GoogleApi((LocationManager) getSystemService(Context.LOCATION_SERVICE));
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(MapsActivity.this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        ((GoogleApi) mapApi).setMap(googleMap);
     }
 
-    public void updatePosition() {
-        if (marker != null) {
-            marker.remove();
-        }
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
-            return;
-        }
-
-        for (String locManager : locationManager.getAllProviders()) {
-            locationManager.requestLocationUpdates(locManager, (long) listenTime, (float) listenDistance, locationListener);
-        }
-
-        bestProvider = locationManager.getBestProvider(criteria, true);
-        currentLocation = locationManager.getLastKnownLocation(bestProvider);
-        if (currentLocation == null) {
-            return;
-        }
-        
-        LatLng myPos = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        marker = mMap.addMarker(new MarkerOptions().position(myPos).title("My position"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPos));
-    }
-
-    @Override
-    public Location getCurrentLocation() {
-        return null;
-    }
 }
