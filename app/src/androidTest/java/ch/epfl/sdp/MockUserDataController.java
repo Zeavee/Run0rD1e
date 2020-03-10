9585c177cddadad34b56273f8bf76eede4970e02
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-03-10 20:44:40 +0100

    new mock for the datacontroller

---
+++ app/src/androidTest/java/ch/epfl/sdp/MockUserDataController.java
@@ -0,0 +1,21 @@
+package ch.epfl.sdp;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MockUserDataController implements UserDataController{
+    private TreeMap<String, Map<String, Object>> userData = new TreeMap<>();
+
+    @Override
+    public Map<String, Object> getUserData(String id) {
+        if(!userData.containsKey(id)) {
+            userData.put(id, new TreeMap<String, Object>());
+        }
+        return userData.get(id);
+    }
+
+    @Override
+    public void setUserAttribute(String id, String attribute, Object value) {
+        getUserData(id).put(attribute, value);
+    }
+}
