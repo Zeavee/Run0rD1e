b25f8ae955885c0ebe87c0984e646abc21db3511
coppey <sacha.coppey@epfl.ch>
2020-03-13 00:45:56 +0100

    doc for mapapi

--- app/src/main/java/ch/epfl/sdp/MapApi.java
+++ app/src/main/java/ch/epfl/sdp/MapApi.java
@@ -6,13 +6,33 @@ import java.util.List;
 
 public interface MapApi {
 
+    /**
+     * Method for the current location
+     * @return the current location of the phone
+     */
     public GeoPoint getCurrentLocation();
 
+    /**
+     * Method that update position, will maybe be switched to private
+     */
     public void updatePosition();
 
+    /**
+     * Displays a list of enemies on the map
+     * @param enemies a list of enemies to display
+     */
     public void displayEnemies(List<Enemy> enemies);
 
+    /**
+     * A method that moves the camera on the current location of the phone
+     */
     public void moveCameraOnCurrentLocation();
 
+    /**
+     * A method that creates a small circle that has always the same size on screen,
+     * so we can see it even when the map is not zoomed
+     * @param color a color in RGB
+     * @return a bitmap, which is an image
+     */
     public Bitmap createSmallCircle(int color);
 }
