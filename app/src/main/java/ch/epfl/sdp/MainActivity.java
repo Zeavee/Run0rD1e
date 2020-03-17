524672e813a2e6e3674ba2c41b09f18b2d13509f
Raphael <raphael.bonatti@epfl.ch>
2020-03-17 22:21:21 +0100

    Instantiate Game into the MainActivity

--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -1,18 +1,25 @@
 package ch.epfl.sdp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import ch.epfl.sdp.game.Game;
+
 public class MainActivity extends AppCompatActivity {
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Lauches the game loop in another thread, must be destroyed at the end
+        game = new Game();
+        game.initGame();
 
         // Locate the button in activity_main.xml
         Button healthPointButton = findViewById(R.id.mainGoButton);
@@ -40,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void logout(View view) {
+        // Stops the game loop and kills the thread
+        game.destroyGame();
+
         LoginFormActivity.authenticationController.signOut();
         startActivity(new Intent(MainActivity.this, LoginFormActivity.class));
         finish();
