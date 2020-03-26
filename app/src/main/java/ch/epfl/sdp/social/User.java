c5b40d2666bb0ff5209d583b731c1be8b64044ba
Amro Abdrabo <amro.abdrabo@gmail.com>
2020-03-26 17:36:01 +0100

    add date converter for sql room to work

--- app/src/main/java/ch/epfl/sdp/social/User.java
+++ app/src/main/java/ch/epfl/sdp/social/User.java
@@ -3,13 +3,8 @@ package ch.epfl.sdp.social;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
-import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import ch.epfl.sdp.database.UserForFirebase;
 @Entity
 public class User {
     @PrimaryKey
