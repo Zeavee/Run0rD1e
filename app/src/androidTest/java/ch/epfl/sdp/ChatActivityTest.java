52e8579e83fdadd99aae565bd45e46a7f854d732
Amro Abdrabo <amro.abdrabo@gmail.com>
2020-03-27 07:15:41 +0100

    codeclimate hotfix #1

--- app/src/androidTest/java/ch/epfl/sdp/ChatActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/ChatActivityTest.java
@@ -55,30 +55,6 @@ public class ChatActivityTest {
 
     }
 
-    public static class MyViewAction {
-
-        public static ViewAction clickChildViewWithId(final int id) {
-            return new ViewAction() {
-                @Override
-                public Matcher<View> getConstraints() {
-                    return null;
-                }
-
-                @Override
-                public String getDescription() {
-                    return "Click on a child view with specified id.";
-                }
-
-                @Override
-                public void perform(UiController uiController, View view) {
-                    View v = view.findViewById(id);
-                    v.performClick();
-                }
-            };
-        }
-
-    }
-
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
