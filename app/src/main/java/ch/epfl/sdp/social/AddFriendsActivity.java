225e98d47c23b4c095d62356b1ddfd111ea58bf8
Amro Abdrabo <amro.abdrabo@gmail.com>
2020-04-02 10:07:10 +0200

    frontend of add friend done, backend to be done now

--- app/src/main/java/ch/epfl/sdp/social/AddFriendsActivity.java
+++ app/src/main/java/ch/epfl/sdp/social/AddFriendsActivity.java
@@ -1,9 +1,18 @@
 package ch.epfl.sdp.social;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ch.epfl.sdp.R;
 
@@ -12,19 +21,55 @@ public class AddFriendsActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerQueryAdapter adapter;
-
+    private List<String> moviesList;
+    private Toolbar mActionBarToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friends);
 
+        
+        
         recyclerView = findViewById(R.id.recyclerQueryFriends);
-        adapter = new RecyclerQueryAdapter();
+        moviesList = new ArrayList<>();
+        moviesList.add("ehllo");
+        moviesList.add("sami");
+        adapter = new RecyclerQueryAdapter(moviesList);
+
 
         // layoutManager of recyclerView implemented in the xml file
+        
         recyclerView.setAdapter(adapter);
+        DividerItemDecoration decoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(decoration);
+
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu_addfriend, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            // perform firestore search here
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
--- app/src/main/java/ch/epfl/sdp/social/RecyclerQueryAdapter.java
+++ app/src/main/java/ch/epfl/sdp/social/RecyclerQueryAdapter.java
@@ -1,17 +1,36 @@
 package ch.epfl.sdp.social;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import ch.epfl.sdp.R;
 
-public class RecyclerQueryAdapter  extends RecyclerView.Adapter<RecyclerQueryAdapter.ViewHolder> {
+public class RecyclerQueryAdapter  extends RecyclerView.Adapter<RecyclerQueryAdapter.ViewHolder> implements Filterable {
+
+    private List<String> friendsList;
+
+    //movies but without being filtered
+    private List<String> friendsAll;
+
+    public RecyclerQueryAdapter(List<String> moviesList) {
+        this.friendsList = moviesList;
+        this.friendsAll = new ArrayList<>(moviesList);
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -24,14 +43,60 @@ public class RecyclerQueryAdapter  extends RecyclerView.Adapter<RecyclerQueryAda
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        holder.usernameTextView.setText(String.valueOf(position));
+        holder.emailTextView.setText(friendsList.get(position));
+
+
     }
 
     @Override
     public int getItemCount() {
-        return 20;
+        return friendsList.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+    Filter filter = new Filter() {
+
+        // run on background thread
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+
+            List<String> filteredList = new ArrayList<>();
+            if (constraint.toString().isEmpty())
+            {
+                filteredList.addAll(friendsAll);
+            }else {
+                for (String friend: friendsAll)
+                {
+                    if (friend.toLowerCase().contains(constraint.toString().toLowerCase()))
+                    {
+                        filteredList.add(friend);
+                    }
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filteredList;
+
+            return filterResults;
+        }
+
+        // runs on UI thread
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            friendsList.clear();
+            friendsList.addAll((Collection<? extends String>) results.values);
+            Log.d("CONSTAINT IS", constraint.toString());
+            Log.d("SIZE isssssssss  ", " "+friendsList.size());
+            for (int i=0;i< friendsList.size();++i) {
+                Log.d("VALUES OF LIST ARE ", i+" "+friendsList.get(i));
+            }
+            notifyDataSetChanged();
+        }
+    };
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
     {
 
         private ImageView imageView;
@@ -42,6 +107,22 @@ public class RecyclerQueryAdapter  extends RecyclerView.Adapter<RecyclerQueryAda
             imageView = itemView.findViewById(R.id.imageView5);
             emailTextView = itemView.findViewById(R.id.textViewEmail);
             usernameTextView = itemView.findViewById(R.id.textViewUsername);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(new View.OnLongClickListener(){
+
+                @Override
+                public boolean onLongClick(View v) {
+                    friendsList.remove(getAdapterPosition());
+                    notifyItemRemoved(getAdapterPosition());
+                    return true;
+                }
+            });
+        }
+
+        /// here is where you add that the user become friends in both firestore and sqlite
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(v.getContext(), friendsList.get(getAdapterPosition()), Toast.LENGTH_SHORT).show();
         }
     }
 }
