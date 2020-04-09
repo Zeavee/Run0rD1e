ab44652a88a877c12a601e158bffc549a40bf177
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-04-09 17:56:21 +0200

    wrote a test for leaderboardActivitTest

--- app/src/androidTest/java/ch/epfl/sdp/LeaderboardActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/LeaderboardActivityTest.java
@@ -1,4 +1,24 @@
 package ch.epfl.sdp;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import ch.epfl.sdp.leaderboard.LeaderboardActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
 public class LeaderboardActivityTest {
+    @Rule
+    public final ActivityTestRule<LeaderboardActivity> mActivityRule =
+            new ActivityTestRule<>(LeaderboardActivity.class);
+
+    @Test
+    public void onCreate() {
+        onView(withId(R.id.recycler_view)).perform();
+    }
 }
