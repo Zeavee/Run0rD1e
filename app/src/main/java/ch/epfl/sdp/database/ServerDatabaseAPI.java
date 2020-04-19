5707e8d3bbb64c1d9c9f78180df923c16b12a27f
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-04-19 17:13:50 +0200

    tried to change

--- app/src/main/java/ch/epfl/sdp/database/ServerDatabaseAPI.java
+++ app/src/main/java/ch/epfl/sdp/database/ServerDatabaseAPI.java
@@ -6,7 +6,7 @@ import ch.epfl.sdp.artificial_intelligence.Enemy;
 import ch.epfl.sdp.entity.Player;
 import ch.epfl.sdp.item.ItemBox;
 
-public interface ServerDatabaseAPI extends UserDataController{
+public interface ServerDatabaseAPI extends UserDataController {
     void sendEnemies(List<Enemy> enemies);
     void sendDamage(List<Player> players);
     void sendItemBox(ItemBox itemBox);
