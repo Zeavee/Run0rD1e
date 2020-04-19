14fc65040792c6c6d2b267229002bf02f9be8ff8
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-04-19 17:13:13 +0200

    implemented a part of enemy in Server class

--- app/src/main/java/ch/epfl/sdp/game/Server.java
+++ app/src/main/java/ch/epfl/sdp/game/Server.java
@@ -1,14 +1,36 @@
 package ch.epfl.sdp.game;
 
+import ch.epfl.sdp.artificial_intelligence.Enemy;
+import ch.epfl.sdp.artificial_intelligence.EnemyGenerator;
+import ch.epfl.sdp.artificial_intelligence.EnemyManager;
 import ch.epfl.sdp.artificial_intelligence.Updatable;
-import ch.epfl.sdp.entity.Player;
 
+/**
+ * Takes care of all actions that a server should perform (generating enemies, updating enemies etc.).
+ */
 public class Server implements Updatable {
-    Server(){
+    public final int GENERATE_ENEMY_EVERY_MS = 10000;
 
+    private long lastEnemyGenerateTimeMillis = System.currentTimeMillis();
+
+    private EnemyManager manager;
+    private EnemyGenerator enemyGenerator;
+
+    public Server(EnemyManager manager, EnemyGenerator enemyGenerator) {
+        this.manager = manager;
+        this.enemyGenerator = enemyGenerator;
     }
 
     @Override
     public void update() {
+        long currentTimeMillis = System.currentTimeMillis();
+        if(currentTimeMillis - lastEnemyGenerateTimeMillis >= GENERATE_ENEMY_EVERY_MS) {
+            //Enemy enemy = enemyGenerator.generateEnemy(100);
+            Enemy enemy = new Enemy();
+            lastEnemyGenerateTimeMillis = currentTimeMillis;
+            manager.addEnemy(enemy);
+        }
+
+        manager.update();
     }
 }
