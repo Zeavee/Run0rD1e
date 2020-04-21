a4b29050fc6b410d0b69c0a86ae7686390dfeb3c
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-04-21 18:24:32 +0200

    refactored leaderboard package

--- app/src/main/java/ch/epfl/sdp/leaderboard/LeaderboardActivity.java
+++ app/src/main/java/ch/epfl/sdp/leaderboard/LeaderboardActivity.java
@@ -3,21 +3,19 @@ package ch.epfl.sdp.leaderboard;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.List;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
-
 import ch.epfl.sdp.R;
 import ch.epfl.sdp.database.firebase.CommonDatabaseAPI;
 import ch.epfl.sdp.database.firebase.CommonFirestoreDatabaseAPI;
 import ch.epfl.sdp.database.room.LeaderboardEntity;
-import ch.epfl.sdp.database.room.LeaderoardViewModel;
 
 public class LeaderboardActivity extends AppCompatActivity {
-    private LeaderoardViewModel leaderoardViewModel;
+    private LeaderboardViewModel leaderboardViewModel;
     public CommonDatabaseAPI commonDatabaseAPI;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +28,19 @@ public class LeaderboardActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // Get a new or existing ViewModel from the ViewModelProvider.
-        leaderoardViewModel = new ViewModelProvider(this).get(LeaderoardViewModel.class);
+        leaderboardViewModel = new ViewModelProvider(this).get(LeaderboardViewModel.class);
 
         // Add an observer on the LiveData.
         // The onChanged() method fires when the observed data changes and the activity is
         // in the foreground.
-        leaderoardViewModel.getLeaderboard().observe(this, users -> {
+        leaderboardViewModel.getLeaderboard().observe(this, users -> {
             // Update the cached copy of the words in the adapter.
             adapter.setLeaderboard(users);
         });
-        leaderoardViewModel.getLeaderboard().observe(this, users -> setupChampions(users));
+        leaderboardViewModel.getLeaderboard().observe(this, users -> setupChampions(users));
 
         commonDatabaseAPI = new CommonFirestoreDatabaseAPI();
-        commonDatabaseAPI.syncCloudFirebaseToRoom(leaderoardViewModel);
+        commonDatabaseAPI.syncCloudFirebaseToRoom(leaderboardViewModel);
     }
 
     private void setupChampions(List<LeaderboardEntity> users) {
--- app/src/main/java/ch/epfl/sdp/leaderboard/LeaderboardViewModel.java
+++ app/src/main/java/ch/epfl/sdp/leaderboard/LeaderboardViewModel.java
@@ -1,18 +1,20 @@
-package ch.epfl.sdp.database.room;
+package ch.epfl.sdp.leaderboard;
 
 import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import ch.epfl.sdp.database.room.DataRepository;
+import ch.epfl.sdp.database.room.LeaderboardEntity;
 
 import java.util.List;
 
-public class LeaderoardViewModel extends AndroidViewModel {
+public class LeaderboardViewModel extends AndroidViewModel {
     private DataRepository sRepository;
     private final LiveData<List<LeaderboardEntity>> mObservableUsers;
 
-    public LeaderoardViewModel(@NonNull Application application) {
+    public LeaderboardViewModel(@NonNull Application application) {
         super(application);
         sRepository = DataRepository.getInstance(application);
         mObservableUsers = sRepository.getLeaderboard();
