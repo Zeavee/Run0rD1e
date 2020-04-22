0fe880431f11c9e86371456512e9906db0bb8729
Zeavee <futur999@bluewin.ch>
2020-04-22 17:58:59 +0200

    add a javadoc for the method of Market and add a check when we buy (to be sure the item is in stock)

--- app/src/main/java/ch/epfl/sdp/item/Market.java
+++ app/src/main/java/ch/epfl/sdp/item/Market.java
@@ -14,12 +14,18 @@ import ch.epfl.sdp.map.Displayable;
 import ch.epfl.sdp.map.GeoPoint;
 import ch.epfl.sdp.map.MapsActivity;
 
+/**
+ * A Market is a place where the players can go and buy items using their money
+ */
 public class Market extends InteractiveEntity implements Displayable {
     private Map<Item, Pair<Integer, Integer>> stock;
     private final double MIN_PRICE = 200;
     private final double MAX_PRICE = 300;
     private final RandomGenerator randomGenerator = new RandomGenerator();
 
+    /**
+     * This is a constructor for Market which randomly initialize the items that will be available
+     */
     public Market() {
         super(EntityType.MARKET, RandomGenerator.randomLocationOnCircle(MapsActivity.mapApi.getCurrentLocation(), 1000), true);
         stock = new HashMap<>();
@@ -29,11 +35,24 @@ public class Market extends InteractiveEntity implements Displayable {
         }
     }
 
+    /**
+     * A method to get the stock of the Market
+     * @return an unmodifiable map that show the items we can buy, their quantity and their price
+     */
     public Map<Item, Pair<Integer, Integer>> getStock() {
         return Collections.unmodifiableMap(stock);
     }
 
+    /**
+     * A method to buy an item from the Market
+     * @param item The item the player wants to buy. If the item is not in the stock, we return false
+     * @param player The player that wants to buy the item
+     * @return a boolean that shows if the transaction occurred correctly
+     */
     public boolean buy(Item item, Player player) {
+        if (!stock.containsKey(item)) {
+            return false;
+        }
         int currentStock = stock.get(item).first;
         int price = stock.get(item).second;
         if (currentStock <= 0 || player.getMoney() < price || !player.removeMoney(price)) {
