fc69c736122734252e4055e3dfc068c7a2431912
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-04-22 15:04:14 +0200

    replaced initEnv

--- app/src/main/java/ch/epfl/sdp/game/Server.java
+++ app/src/main/java/ch/epfl/sdp/game/Server.java
@@ -1,8 +1,18 @@
 package ch.epfl.sdp.game;
 
+import java.util.List;
+
 import ch.epfl.sdp.artificial_intelligence.Enemy;
 import ch.epfl.sdp.artificial_intelligence.EnemyGenerator;
 import ch.epfl.sdp.artificial_intelligence.EnemyManager;
+import ch.epfl.sdp.artificial_intelligence.SinusoidalMovement;
+import ch.epfl.sdp.geometry.GeoPoint;
+import ch.epfl.sdp.geometry.LocalArea;
+import ch.epfl.sdp.geometry.PointConverter;
+import ch.epfl.sdp.geometry.RectangleArea;
+import ch.epfl.sdp.geometry.UnboundedArea;
+import ch.epfl.sdp.item.Healthpack;
+import ch.epfl.sdp.item.ItemBox;
 
 /**
  * Takes care of all actions that a server should perform (generating enemies, updating enemies etc.).
@@ -14,10 +24,40 @@ public class Server implements Updatable {
 
     private EnemyManager manager;
     private EnemyGenerator enemyGenerator;
+    private double damage;
+    private List<ItemBox> itemBoxes;
 
     public Server(EnemyManager manager, EnemyGenerator enemyGenerator) {
         this.manager = manager;
         this.enemyGenerator = enemyGenerator;
+    }
+
+    public static void initEnvironment() {
+        Game.getInstance().initGame();
+
+        // Enemy -------------------------------------------
+        GeoPoint local = new GeoPoint(6.2419, 46.2201);
+        GeoPoint enemyPos = new GeoPoint(6.3419, 46.2301);
+        LocalArea localArea = new LocalArea(new RectangleArea(3500, 3500), PointConverter.geoPointToCartesianPoint(local));
+        Enemy enemy = new Enemy(localArea, new UnboundedArea());
+        enemy.setLocation(enemyPos);
+        SinusoidalMovement movement = new SinusoidalMovement(PointConverter.geoPointToCartesianPoint(enemyPos));
+        movement.setVelocity(5);
+        movement.setAngleStep(0.1);
+        movement.setAmplitude(10);
+        enemy.setMovement(movement);
+        Game.getInstance().addToDisplayList(enemy);
+        Game.getInstance().addToUpdateList(enemy);
+        //  -------------------------------------------
+
+        // ItemBox -------------------------------------------
+        Healthpack healthpack = new Healthpack(10);
+        ItemBox itemBox = new ItemBox();
+        itemBox.putItems(healthpack, 1);
+        itemBox.setLocation(new GeoPoint(6.14, 46.22));
+        Game.getInstance().addToDisplayList(itemBox);
+        Game.getInstance().addToUpdateList(itemBox);
+        //  -------------------------------------------
     }
 
     @Override
@@ -32,4 +72,6 @@ public class Server implements Updatable {
 
         manager.update();
     }
+
+
 }
--- app/src/main/java/ch/epfl/sdp/map/MapsActivity.java
+++ app/src/main/java/ch/epfl/sdp/map/MapsActivity.java
@@ -6,33 +6,21 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.fragment.app.FragmentActivity;
-
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import androidx.fragment.app.FragmentActivity;
 import ch.epfl.sdp.R;
-import ch.epfl.sdp.artificial_intelligence.Enemy;
-import ch.epfl.sdp.artificial_intelligence.SinusoidalMovement;
 import ch.epfl.sdp.database.firebase.api.CommonDatabaseAPI;
 import ch.epfl.sdp.database.firebase.entity.EntityConverter;
 import ch.epfl.sdp.database.firebase.entity.PlayerForFirebase;
-import ch.epfl.sdp.database.firebase.entity.UserForFirebase;
 import ch.epfl.sdp.entity.Player;
 import ch.epfl.sdp.entity.PlayerManager;
-import ch.epfl.sdp.game.Game;
-import ch.epfl.sdp.geometry.GeoPoint;
-import ch.epfl.sdp.geometry.LocalArea;
-import ch.epfl.sdp.geometry.PointConverter;
-import ch.epfl.sdp.geometry.RectangleArea;
-import ch.epfl.sdp.geometry.UnboundedArea;
-import ch.epfl.sdp.item.Healthpack;
-import ch.epfl.sdp.item.ItemBox;
+import ch.epfl.sdp.game.Server;
 import ch.epfl.sdp.login.AuthenticationAPI;
 import ch.epfl.sdp.utils.DependencyFactory;
 
@@ -62,33 +50,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(MapsActivity.this);
     }
 
-    private void initEnvironment() {
-        Game.getInstance().initGame();
 
-        // Enemy -------------------------------------------
-        GeoPoint local = new GeoPoint(6.2419, 46.2201);
-        GeoPoint enemyPos = new GeoPoint(6.3419, 46.2301);
-        LocalArea localArea = new LocalArea(new RectangleArea(3500, 3500), PointConverter.geoPointToCartesianPoint(local));
-        Enemy enemy = new Enemy(localArea, new UnboundedArea());
-        enemy.setLocation(enemyPos);
-        SinusoidalMovement movement = new SinusoidalMovement(PointConverter.geoPointToCartesianPoint(enemyPos));
-        movement.setVelocity(5);
-        movement.setAngleStep(0.1);
-        movement.setAmplitude(10);
-        enemy.setMovement(movement);
-        Game.getInstance().addToDisplayList(enemy);
-        Game.getInstance().addToUpdateList(enemy);
-        //  -------------------------------------------
-
-        // ItemBox -------------------------------------------
-        Healthpack healthpack = new Healthpack(10);
-        ItemBox itemBox = new ItemBox();
-        itemBox.putItems(healthpack, 1);
-        itemBox.setLocation(new GeoPoint(6.14, 46.22));
-        Game.getInstance().addToDisplayList(itemBox);
-        Game.getInstance().addToUpdateList(itemBox);
-        //  -------------------------------------------
-    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -119,7 +81,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             if (!registerToLobbyRes.isSuccessful()) {
                                 Toast.makeText(MapsActivity.this, registerToLobbyRes.getException().getMessage(), Toast.LENGTH_LONG).show();
                             } else {
-                                initEnvironment();
+                                Server.initEnvironment();
                             }
                         });
                     }
