2f5cb63a7783c7e85667c8e095a626b372f44b7c
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-05-06 22:22:16 +0200

    fixed mainActivityTest

--- app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -1,5 +1,5 @@
-//package ch.epfl.sdp;
-/*
+package ch.epfl.sdp;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -32,11 +32,11 @@ public class MainActivityTest {
         Game.getInstance().setMapApi(new MockMapApi());
         PlayerManager.getInstance().setCurrentUser(new Player("test", "test@gmail.com"));
     }
-
+/*
     @After
     public void teardown(){
         PlayerManager.getInstance().setCurrentUser(null);
-    }
+    }*/
 
     @Test
     public void rulesOpens() {
@@ -57,8 +57,8 @@ public class MainActivityTest {
         onView(withId(R.id.map)).check(matches(isDisplayed()));
     }*/
 
-   /* @Test
+   @Test
     public void leaderboardOpens() {
         onView(withId(R.id.leaderboard)).perform(click());
     }
-}*/
+}
