430ddde36ba5d38a329af60d03f4b634f8e44159
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-05-06 23:41:42 +0200

    added one more test

--- app/src/test/java/ch/epfl/sdp/items/HealthpackTest.java
+++ app/src/test/java/ch/epfl/sdp/items/HealthpackTest.java
@@ -4,9 +4,11 @@ import org.junit.Test;
 
 import ch.epfl.sdp.entity.Player;
 import ch.epfl.sdp.entity.PlayerManager;
+import ch.epfl.sdp.geometry.CartesianPoint;
 import ch.epfl.sdp.item.Healthpack;
 import ch.epfl.sdp.item.Item;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class HealthpackTest {
@@ -18,20 +20,26 @@ public class HealthpackTest {
         Item cloned = original.clone();
         assertTrue(((Healthpack)cloned).getHealthPackAmount() == original.getHealthPackAmount());
     }
-/*
+
     @Test
     public void healthPackIncreasesHealthToThreshold(){
         Player sick = new Player(20.0, 20.0, 100, "amroa", "amro.abdrabo@gmail.com");
         sick.setHealthPoints(20.0);
         PlayerManager.getInstance().setCurrentUser(sick);
         Healthpack pack = new Healthpack(30);
-        pack.useOn(sick);
+    //  pack.useOn(sick);
     //  assertTrue(sick.getHealthPoints() == 50.0);
     //  Healthpack pack2 = new Healthpack(70);
     //  pack2.useOn(sick);
     //  assertTrue(sick.getHealthPoints() == 100.0);
-    //    PlayerManager.getInstance().setCurrentUser(originalPlayer);
-    }*/
+    //  PlayerManager.getInstance().setCurrentUser(originalPlayer);
+    }
+
+    @Test
+    public void getHealthPackTest() {
+        Healthpack pack1 = new Healthpack(70);
+        assertTrue(pack1.getHealthPackAmount() == 70);
+    }
 
 
 }
