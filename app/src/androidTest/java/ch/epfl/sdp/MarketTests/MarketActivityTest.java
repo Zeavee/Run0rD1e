d6f869d024a7301e02897fffe35a3d4abe9f5ee5
Amro Abdrabo <amro.abdrabo@gmail.com>
2020-05-08 20:27:09 +0200

    remove auto generated child matcher

--- app/src/androidTest/java/ch/epfl/sdp/MarketTests/MarketActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/MarketTests/MarketActivityTest.java
@@ -41,6 +41,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static ch.epfl.sdp.SocialTests.ChildParentMatcher.childAtPosition;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
@@ -251,22 +252,4 @@ public class MarketActivityTest {
         step9();
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
 }
