fce7f8ef7c1a49f384e5338dbd5f7955a283ac80
Raphael Bonatti <raphael.bonatti@epfl.ch>
2020-05-10 16:18:48 +0200

    Adapt PlayerManager

--- app/src/main/java/ch/epfl/sdp/entity/PlayerManager.java
+++ app/src/main/java/ch/epfl/sdp/entity/PlayerManager.java
@@ -13,9 +13,9 @@ import ch.epfl.sdp.geometry.GeoPoint;
  * beginning of each game and all players should be removed at the end of each game.
  */
 public class PlayerManager {
-    public static final int NUMBER_OF_PLAYERS_IN_LOBBY = 2;
+    public static final int NUMBER_OF_PLAYERS_IN_LOBBY = 1;
     public static final String USER_COLLECTION_NAME = "AllUsers";
-    public static final String LOBBY_COLLECTION_NAME = "RaphaelLobbies";
+    public static final String LOBBY_COLLECTION_NAME = "Lobbies";
     public static final String PLAYER_COLLECTION_NAME = "Players";
     public static final String ENEMY_COLLECTION_NAME = "Enemies";
     public static final String ITEM_COLLECTION_NAME = "Items";
