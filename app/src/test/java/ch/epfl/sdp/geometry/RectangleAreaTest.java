5aca4106eba6cdb43d75e81505e23020c913ba78
Zeavee <futur999@bluewin.ch>
2020-05-13 21:11:23 +0200

    fix EnemyGeneratorTest

--- app/src/test/java/ch/epfl/sdp/geometry/RectangleAreaTest.java
+++ app/src/test/java/ch/epfl/sdp/geometry/RectangleAreaTest.java
@@ -2,14 +2,12 @@ package ch.epfl.sdp.geometry;
 
 import org.junit.Test;
 
-import ch.epfl.sdp.geometry.RectangleArea;
-
 import static org.junit.Assert.assertTrue;
 
 public class RectangleAreaTest {
     @Test
     public void testGetters() {
-        RectangleArea rectangleArea = new RectangleArea(10, 20);
+        RectangleArea rectangleArea = new RectangleArea(10, 20, new GeoPoint(10, 20));
         assertTrue(rectangleArea.getHeight() == 10);
         assertTrue(rectangleArea.getWidth() == 20);
     }
