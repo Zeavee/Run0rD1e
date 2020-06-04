5daf734056a9fc0bfac1858cc54cd663fbd2f9c3
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2020-06-04 07:41:53 +0200

    Rule activities pages unit tests

---
+++ app/src/androidTest/java/ch/epfl/sdp/logic/RulesActivityPage1Test.java
@@ -0,0 +1,28 @@
+package ch.epfl.sdp.logic;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import ch.epfl.sdp.R;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class RulesActivityPage1Test {
+    @Rule
+    public ActivityTestRule<RulesActivityPage1> mActivityRule = new IntentsTestRule<>(RulesActivityPage1.class);
+
+    @Test
+    public void btnPage2Click_OpensRulesActivityPage2() {
+        onView(withId(R.id.btnPage2)).perform(click());
+        intended(hasComponent(RulesActivityPage2.class.getName()));
+    }
+}
